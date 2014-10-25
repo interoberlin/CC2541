c980298923320626bd3fa48ce465ad452e5c33f1
Matthias <mail@matthiasbock.net>
2014-10-25 22:49:18 +0200

    open serial port

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 ble: ble.c
-	gcc -Wall ble.c -o ble
+	gcc ble.c -o ble
 clean:
 	@rm ble
 
--- ble.c
+++ ble.c
@@ -1,27 +1,58 @@
 
-// http://en.wikibooks.org/wiki/Serial_Programming/termios
-
+#include <stdlib.h>
 #include <stdio.h>
-#include <fcntl.h>
 #include <termios.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-struct termios config;
+int main() {
+	int ttyfd = open("/dev/ttyAMA0", O_RDWR);
+	printf("fd: %d\n", ttyfd);
 
-const char *device = "/dev/ttyAMA0";
-int fd;
-fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
-if (fd == -1)
-{
-    printf("Error: Failed to open port.\n");
+	struct termios attribs;
+    speed_t speed;
+    /*
+     * Get the current settings. This saves us from
+     * having to initialize a struct termios from
+     * scratch.
+     */
+    if(tcgetattr(ttyfd, &attribs) < 0)
+    {
+        perror("stdin");
+        return EXIT_FAILURE;
+    }
+    /*
+     * Set the speed data in the structure
+     */
+    if(cfsetispeed(&attribs, B9600) < 0)
+    {
+        perror("invalid baud rate");
+        return EXIT_FAILURE;
+    }
+
+if(tcsetattr(ttyfd, TCSANOW, &attribs) < 0)
+    {
+        perror("stdin");
+        return EXIT_FAILURE;
+    }
+
+        int res, STOP = 0;
+	char buf[255];
+        while(STOP == 0)
+        {
+	write(ttyfd, "AT");
+            while((res = read(ttyfd,buf,255)) == 0);
+            {
+                if(res > 0)
+                {
+                    buf[res]=0;
+                    printf("%s:%d\n", buf, res);
+                    if(buf[sizeof(buf)]=='\n') break;   
+                }
+            }
+        }
+	
+	return 0;
 }
 
-if (cfsetispeed(&config, B9600) < 0 || cfsetospeed(&config, B9600) < 0)
-{
-    printf("Error: Setting baud failed.\n");
-}
-
-write(fd, "AT");
-printf("%s\n", read(fd));
-
-close(fd);
